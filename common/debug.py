f2ed26e3d67002574ccc8960872b8e712df84e2f
Matthias Dellweg <2500@gmx.de>
2017-08-29 22:26:58 +0200

    use decorator in debug.py

--- common/debug.py
+++ common/debug.py
@@ -1,13 +1,14 @@
 # Debug decorator
 
-def debug_me(func):
+from decorator import decorator
+
+@decorator
+def debug_me(func, *args, **kwargs):
     """
         Decorator that prints function name before calling the actual function
     """
-    def lamb(*args, **kwargs):
-        arg_str = ", ".join([str(a) for a in args] + [str(k) + "=" + str(kwargs[k]) for k in kwargs])
-        print('#--- {}({}) ---'.format(func.__name__, arg_str))
-        result = func(*args, **kwargs)
-        print('#--- RES: {} ---'.format(result))
-        return result
-    return lamb
+    arg_str = ", ".join([str(a) for a in args] + [str(k) + "=" + str(kwargs[k]) for k in kwargs])
+    print('#--- {}({}) ---'.format(func.__name__, arg_str))
+    result = func(*args, **kwargs)
+    print('#--- RES: {} ---'.format(result))
+    return result
