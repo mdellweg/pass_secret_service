70f89559315e17df6dbfceea6a1e8606a1257f2d
Matthias Dellweg <2500@gmx.de>
2017-10-22 20:27:25 +0200

    Prepare tests for use of pypass
    Signed-off-by: Matthias Dellweg <2500@gmx.de>

--- tests/pass_run_session.sh
+++ tests/pass_run_session.sh
@@ -7,22 +7,23 @@ export PYTHONPATH=$PYTHONPATH:$(dirname $REALDIR)
 
 teardown()
 {
-	#echo "# --- teardown ---"
-	kill $SERVICE_PID
-	#echo "#  stopped ${SERVICE_PID}"
+	kill $SERVICE_PID || true
+	rm -r $PASSWORD_STORE_DIR || true
 }
 
 setup()
 {
-	#echo "# --- setup ---"
-	python3 -m coverage run $REALDIR/../pass_secret_service.py &
+	# prepare test data
+	PASSWORD_STORE_DIR=$REALDIR/../.test-password-store
+	PASSWORD_STORE_DIR=$PASSWORD_STORE_DIR pass init $GPG_ID
+	echo "password" | PASSWORD_STORE_DIR=$PASSWORD_STORE_DIR pass insert -e secret_service/default/somewhere.example.com
+
+	# run service
+	PASSWORD_STORE_DIR=$PASSWORD_STORE_DIR python3 -m coverage run $REALDIR/../pass_secret_service.py --path $PASSWORD_STORE_DIR &
 	SERVICE_PID=$!
-	#echo "#  Started ${SERVICE_PID}"
 	trap teardown EXIT
 }
 
 setup
-
-# --- run command ---
 
 "$@"
